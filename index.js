31bcafacb85beb175c3ec4a663dab2a0a5da4965
roxiewu <56368545+roxiewu@users.noreply.github.com>
2024-09-21 15:02:55 -0400

    Add console.log to debug

--- index.js
+++ index.js
@@ -5,27 +5,33 @@ let guestScoreEl = document.getElementById("guest-score")
 
 function homeOnePoint(){
     homeScoreEl.innerText = Number(homeScoreEl.innerText) + 1
+    console.log("homeOnePoint")
 }
 
 function homeTwoPoint(){
     homeScoreEl.innerText = Number(homeScoreEl.innerText) + 2
+    console.log("homeTwoPoint")
 }
 
 
 function homeThreePoint(){
     homeScoreEl.innerText = Number(homeScoreEl.innerText) + 3
+    console.log("homeThreePoint")
 }
 
 
 function guestOnePoint(){
     guestScoreEl.innerText = Number(guestScoreEl.innerText) + 1
+    console.log("guestOnePoint")
 }
 
 function guestTwoPoint(){
     guestScoreEl.innerText = Number(guestScoreEl.innerText) + 2
+    console.log("guestTwoPoint")
 }
 
 
 function guestThreePoint(){
     guestScoreEl.innerText = Number(guestScoreEl.innerText) + 3
+    console.log("guestThreePoint")
 }
